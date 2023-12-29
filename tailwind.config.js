bf417ad5d0e1a6e3df016383c4adbf6f3df8c88f
AshwinIyer101 <145373602+AshwinIyer101@users.noreply.github.com>
2023-12-30 02:52:48 +0530

    Added terms and conditions

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
+    fontSize: {
+      xs: '0.55rem',
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+  
     extend: {},
+    }
   },
   plugins: [
     require('flowbite/plugin')
