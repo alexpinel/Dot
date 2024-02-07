018234cbaa6a6b52f259f35d84e69a469909884e
alexpinel <alexpinel28@gmail.com>
2024-02-07 12:26:23 +0000

    Dark mode enhancements

--- src/render.js
+++ src/render.js
@@ -223,7 +223,7 @@ $(document).ready(() => {
                                 )
                                 // SVG for arrow icon
                                 arrow.html(
-                                    `<svg class="w-3 h-3 text-black dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
+                                    `<svg class="w-3 h-3 text-black dark:text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1"/>
                                     </svg>`
                                 )
@@ -265,7 +265,7 @@ $(document).ready(() => {
 
                                 // SVG for document icon
                                 icon.html(
-                                    `<svg class="w-3 h-3 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
+                                    `<svg class="w-3 h-3 text-gray-800 dark:text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
                                     <path stroke="currentColor" stroke-linejoin="round" stroke-width="2" d="M6 1v4a1 1 0 0 1-1 1H1m14-4v16a.97.97 0 0 1-.933 1H1.933A.97.97 0 0 1 1 18V5.828a2 2 0 0 1 .586-1.414l2.828-2.828A2 2 0 0 1 5.828 1h8.239A.97.97 0 0 1 15 2Z"/>
                                   </svg>`
                                 ).addClass('icon  inline-block ml-1')
